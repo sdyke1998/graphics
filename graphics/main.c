a171c2b8de1e0ac618934f8a8d93c8b99bb04ec5
Scott <sdyke1998@gmail.com>
2020-10-26 23:13:08 +0000

    Most tutorial completed except for "Putting it all together part"

--- graphics/main.c
+++ graphics/main.c
@@ -7,7 +7,15 @@ void error_callback(int error, const char* description)
 	fprintf(stderr, "Error: %s\n", description);
 }
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main() {
+
+	double time = glfwGetTime();
 
 	if (!glfwInit())
 	{
@@ -16,12 +24,29 @@ int main() {
 
 	glfwSetErrorCallback(error_callback);
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
 	if (!window)
 	{
 		// Window or OpenGL context creation failed
 	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
 
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	glfwSwapBuffers(window);
+	glfwSwapInterval(1);
+	glfwPollEvents();
+
+	while (!glfwWindowShouldClose(window)) {
+		//Keep running
+	}
+
+	glfwDestroyCursor(window);
 	glfwTerminate();
 	return 0;
 }
